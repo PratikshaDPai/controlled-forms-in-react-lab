9014d5fa76b9b0b5fdef70a45d37b867b13cbf7b
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-27 19:51:10 -0700

    Add bookshelf template

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function BookShelf() {
+  return (
+    <div className="bookshelfDiv">
+      <div className="formDiv">
+        <h3>Add a Book</h3>
+        {}
+      </div>
+
+      <div className="bookCardsDiv">{}</div>
+    </div>
+  );
+}
+
+export default Bookshelf;
