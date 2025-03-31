5813f501543165d9c73c8e384e78524903d0a9af
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-30 23:00:25 -0700

    Add form logic

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -5,15 +5,56 @@ export default function BookShelf() {
     { title: "Fourth Wing", author: "Rebecca Yarros" },
     { title: "The Lion, the Witch and the Wardrobe", author: "C.S. Lewis" },
   ]);
+  const [formData, setFormData] = useState({ title: "", author: "" });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newBooks = [...books];
+    newBooks.push(formData);
+    setBooks(newBooks);
+  }
+
+  const handleChange = ({ target }) => {
+    setFormData({ ...formData, [target.name]: target.value });
+  };
 
   return (
     <div className="bookshelfDiv">
       <div className="formDiv">
         <h3>Add a Book</h3>
-        {}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={handleChange}
+            value={formData.title}
+            required
+          />
+
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            onChange={handleChange}
+            value={formData.author}
+            required
+          />
+
+          <button type="submit">Add Book</button>
+        </form>
       </div>
 
-      <div className="bookCardsDiv">{}</div>
+      <div className="bookCardsDiv">
+        {books.map((book, index) => (
+          <div className="bookCard" key={index}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
